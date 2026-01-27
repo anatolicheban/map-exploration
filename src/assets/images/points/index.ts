62b76841de5a0e854ffcd87e8bdc3d7efc7041b4
Anatoli Cheban <107035461+anatolicheban@users.noreply.github.com>
2026-01-27 23:51:22 +0100

    new route

--- src/assets/images/points/index.ts
+++ src/assets/images/points/index.ts
@@ -53,3 +53,66 @@ export { default as historical09x1 } from "./historical/09/1.jpg";
 export { default as historical09x2 } from "./historical/09/2.png";
 export { default as historical09x3 } from "./historical/09/3.webp";
 export { default as historical09x4 } from "./historical/09/4.png";
+
+export { default as military01x1 } from "./military/01/1.png";
+export { default as military01x2 } from "./military/01/2.png";
+export { default as military01x3 } from "./military/01/3.png";
+export { default as military01x4 } from "./military/01/4.png";
+export { default as military01x5 } from "./military/01/5.png";
+export { default as military01x6 } from "./military/01/6.png";
+export { default as military01x7 } from "./military/01/7.png";
+export { default as military01x8 } from "./military/01/8.png";
+export { default as military01x9 } from "./military/01/9.png";
+export { default as military01x10 } from "./military/01/10.png";
+export { default as military01x11 } from "./military/01/11.png";
+export { default as military01x12 } from "./military/01/12.png";
+
+export { default as military02x1 } from "./military/02/1.png";
+export { default as military02x2 } from "./military/02/2.png";
+export { default as military02x3 } from "./military/02/3.png";
+
+export { default as military03x1 } from "./military/03/1.png";
+export { default as military03x2 } from "./military/03/2.png";
+
+export { default as military04x1 } from "./military/04/1.png";
+export { default as military04x2 } from "./military/04/2.png";
+export { default as military04x3 } from "./military/04/3.png";
+export { default as military04x4 } from "./military/04/4.png";
+export { default as military04x5 } from "./military/04/5.png";
+export { default as military04x6 } from "./military/04/6.png";
+export { default as military04x7 } from "./military/04/7.png";
+
+export { default as military05x1 } from "./military/05/1.png";
+export { default as military05x2 } from "./military/05/2.png";
+export { default as military05x3 } from "./military/05/3.png";
+export { default as military05x4 } from "./military/05/4.png";
+export { default as military05x5 } from "./military/05/5.png";
+export { default as military05x6 } from "./military/05/6.png";
+
+export { default as military06x1 } from "./military/06/1.png";
+export { default as military06x2 } from "./military/06/2.png";
+export { default as military06x3 } from "./military/06/3.png";
+export { default as military06x4 } from "./military/06/4.png";
+export { default as military06x5 } from "./military/06/5.png";
+export { default as military06x6 } from "./military/06/6.png";
+export { default as military06x7 } from "./military/06/7.png";
+export { default as military06x8 } from "./military/06/8.png";
+
+export { default as military07x1 } from "./military/07/1.png";
+export { default as military07x2 } from "./military/07/2.png";
+export { default as military07x3 } from "./military/07/3.png";
+
+export { default as military08x1 } from "./military/08/1.png";
+export { default as military08x2 } from "./military/08/2.png";
+
+export { default as military09x1 } from "./military/09/1.png";
+export { default as military09x2 } from "./military/09/2.png";
+export { default as military09x3 } from "./military/09/3.png";
+export { default as military09x4 } from "./military/09/4.png";
+export { default as military09x5 } from "./military/09/5.png";
+export { default as military09x6 } from "./military/09/6.png";
+export { default as military09x7 } from "./military/09/7.png";
+export { default as military09x8 } from "./military/09/8.png";
+export { default as military09x9 } from "./military/09/9.png";
+
+export { default as military10x1 } from "./military/10/1.jpg";
